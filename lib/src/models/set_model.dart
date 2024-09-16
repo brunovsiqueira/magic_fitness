d0e36dbcd27adbf08e229e43bec7c63eb0fef895
Bruno Siqueira <brunovsiq@gmail.com>
2024-09-16 21:08:45 +0100

    Refactor models to use Isar

--- lib/src/models/set_model.dart
+++ lib/src/models/set_model.dart
@@ -1,3 +1,6 @@
+import 'package:isar/isar.dart';
+
+@embedded
 class SetModel {
   final String exercise;
   final double weight;
--- lib/src/models/workout_model.dart
+++ lib/src/models/workout_model.dart
@@ -1,12 +1,13 @@
+import 'package:isar/isar.dart';
 import 'package:magic_fitness/src/models/set_model.dart';
 
+@collection
 class WorkoutModel {
-  final String id;
+  final Id id = Isar.autoIncrement;
   final DateTime date;
   final List<SetModel> sets;
 
   WorkoutModel({
-    required this.id,
     required this.date,
     required this.sets,
   });
