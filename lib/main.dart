05572b62c6eda1b2190bca29ea648b0fbea177f0
Bruno Siqueira <brunovsiq@gmail.com>
2024-09-18 19:29:57 +0100

    WIP implementing WorkoutListScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:magic_fitness/src/datasources/workout_datasource.dart';
+import 'package:magic_fitness/src/datasources/workouts_datasource_impl.dart';
+import 'package:magic_fitness/src/models/workout_model.dart';
 import 'package:magic_fitness/src/routes.dart';
 import 'package:magic_fitness/src/screens/add_set_screen.dart';
 import 'package:magic_fitness/src/screens/workout_list_screen.dart';
 import 'package:magic_fitness/src/screens/workout_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const MagicFitnessApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    [WorkoutModelSchema],
+    directory: dir.path,
+  );
+
+  final workoutsDatasource = WorkoutsDatasourceImpl(isar);
+
+  runApp(MagicFitnessApp(
+    workoutsDatasource: workoutsDatasource,
+  ));
+}
 
 class MagicFitnessApp extends StatelessWidget {
-  const MagicFitnessApp({super.key});
+  const MagicFitnessApp({super.key, required this.workoutsDatasource});
+
+  final WorkoutsDatasource workoutsDatasource;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +39,9 @@ class MagicFitnessApp extends StatelessWidget {
       ),
       initialRoute: AppRoutes.workouts,
       routes: {
-        AppRoutes.workouts: (context) => const WorkoutListScreen(),
+        AppRoutes.workouts: (context) => WorkoutListScreen(
+              workoutsDatasource: workoutsDatasource,
+            ),
         AppRoutes.workoutDetails: (context) => WorkoutScreen(),
         AppRoutes.addSet: (context) => const AddSetScreen(),
       },
--- lib/src/screens/workout_list_screen.dart
+++ lib/src/screens/workout_list_screen.dart
@@ -1,49 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:magic_fitness/src/datasources/workout_datasource.dart';
+import 'package:magic_fitness/src/hooks/use_workouts_hooks.dart';
+import 'package:magic_fitness/src/models/workout_model.dart';
 import 'package:magic_fitness/src/routes.dart';
-import 'package:magic_fitness/src/screens/workout_screen.dart';
 
-class WorkoutListScreen extends StatelessWidget {
-  const WorkoutListScreen({super.key});
+class WorkoutListScreen extends HookWidget {
+  final WorkoutsDatasource workoutsDatasource;
+
+  WorkoutListScreen({required this.workoutsDatasource});
 
   @override
   Widget build(BuildContext context) {
+    // Using the custom hook
+    final workoutsData = useWorkouts(workoutsDatasource);
+
     return Scaffold(
       appBar: AppBar(
-          title: const Text(
-        'Workouts',
-      )),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: 3, // Example count
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.only(bottom: 16),
-                    child: ListTile(
-                      title: Text('Workout ${index + 1}'),
-                      subtitle: Text('5 sets • Sept ${10 + index}, 2024'),
-                      trailing: const Icon(Icons.bar_chart, color: Colors.blue),
-                      onTap: () {
-                        //Navigator.pushNamed(context, AppRoutes.workoutDetails, arguments: );
-                      },
-                    ),
+        title: const Text('Workout List'),
+      ),
+      body: ListView.builder(
+        itemCount: workoutsData.workouts.length,
+        itemBuilder: (context, index) {
+          final workout = workoutsData.workouts[index];
+          return ListTile(
+            title: Text(workout.date.toString()),
+            onTap: () async {
+              final updatedWorkout = await Navigator.pushNamed(
+                context,
+                AppRoutes.workoutDetails,
+                arguments: workout,
+              ) as WorkoutModel?;
+
+              if (updatedWorkout != null) {
+                // Update the workout in the local list without refetching
+                workoutsData.updateWorkoutInList(updatedWorkout);
+
+                if (context.mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Workout updated!')),
                   );
-                },
-              ),
-            ),
-          ],
-        ),
+                }
+              }
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => const WorkoutScreen()),
-          // );
+        onPressed: () async {
+          final updatedWorkout =
+              await Navigator.pushNamed(context, AppRoutes.workoutDetails)
+                  as WorkoutModel?;
+          if (updatedWorkout != null) {
+            // Update the workout in the local list without refetching
+            workoutsData.updateWorkoutInList(updatedWorkout);
+
+            if (context.mounted) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Workout updated!')),
+              );
+            }
+          }
         },
         label: const Text('New Workout'),
         icon: const Icon(Icons.add),
